f2769212c973976afbd09999388203092bf6c709
Tuan Huy <huydo.01012003@gmail.com>
2024-09-06 10:57:28 +0700

    feat: config cors

--- src/main/java/com/project/shopapp/config/WebSecurityConfig.java
+++ src/main/java/com/project/shopapp/config/WebSecurityConfig.java
@@ -7,11 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -24,7 +30,6 @@ public class WebSecurityConfig {
     // Lọc request gửi đến
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
@@ -75,6 +80,22 @@ public class WebSecurityConfig {
                             .anyRequest()
                             .authenticated();
 
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(new Customizer<CorsConfigurer<HttpSecurity>>() {
+                    @Override
+                    public void customize(CorsConfigurer<HttpSecurity> httpSecurityCorsConfigurer) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedHeaders(List.of("Authorization", "x-auth-token", "Content-Type"));
+                        corsConfiguration.setAllowedOrigins(List.of("*"));
+                        corsConfiguration.setAllowedMethods(
+                                List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE")
+                        );
+                        corsConfiguration.setExposedHeaders(List.of("x-auth-token"));   // Cấu hình header mà client có thể nhìn thấy sau khi nhận phản hồi từ server
+                        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                        source.registerCorsConfiguration("/**", corsConfiguration); // Mọi url đều được áp dụng
+                        httpSecurityCorsConfigurer.configurationSource(source);
+                    }
                 })
                 .build();
     }
